0d8ab2772c10eb2b46573dcb0c99d1f941831c5c
AmanatUMahmud <amanatmahmud@gmail.com>
2022-10-11 18:24:57 +0600

    made statistics responsive

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,18 +1,20 @@
 import React, { useContext } from 'react';
 import { TopicsContext } from '../layout/Main';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 const Statistics = () => {
     const topics = useContext(TopicsContext);
     // console.log(topics);
 
     return (
-        <div className='mt-5'>
+        <div className='mt-5 ms-5'>
             {/* <LineChart width={600} height={300} data={topics}>
                 <Line type="monotone" dataKey="total" stroke="#8884d8" />
                 <CartesianGrid stroke="#ccc" />
                 <XAxis dataKey="name" />
                 <YAxis dataKey="total" />
             </LineChart> */}
+
+            <ResponsiveContainer width="90%" height={300}>
             <LineChart width={600} height={300} data={topics} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
                 <Line type="monotone" dataKey="total" stroke="#8884d8" />
                 <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
@@ -20,6 +22,7 @@ const Statistics = () => {
                 <YAxis dataKey="total"/>
                 <Tooltip />
             </LineChart>
+            </ResponsiveContainer>
         </div>
     );
 };
