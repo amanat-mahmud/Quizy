a567d3d229b9162dbe77534cc0313727b9c95b61
AmanatUMahmud <amanatmahmud@gmail.com>
2022-10-12 09:10:05 +0600

    options bug fixed

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -3,8 +3,10 @@ const Question = ({ ques, name }) => {
     console.log(ques);
     // const questions = ques.question;
     // const test = questions.split('<')
-    return (
+    const handleClickc =() =>{
 
+    }
+    return (
         <div>
             {/* <h1 className='text-center'>Quiz of {name}</h1> */}
             <h1>{ques.question.replace(/(<([^>]+)>)/ig, '')}</h1>
@@ -18,27 +20,31 @@ const Question = ({ ques, name }) => {
                     </div>
                     <div className="form-check">
                         <input className="form-check-input" type="radio" name={ques.id}
-                        id={ques.id+"1"} value="option2" />
-                        <label className="form-check-label" htmlFor={ques.id+"1"}>
+                            id={ques.id + "1"} value="option2" />
+                        <label className="form-check-label" htmlFor={ques.id + "1"}>
                             {ques.options[1]}
                         </label>
                     </div>
-                    <div className="form-check">
+                    <div className="form-check" 
+                    onClick={handleClickc()}>
                         <input className="form-check-input" type="radio" name={ques.id}
-                         id={ques.id+"2"} value="option3" />
-                        <label className="form-check-label" htmlFor={ques.id+"2"}>
+                            id={ques.id + "2"} value="option3" />
+                        <label className="form-check-label" htmlFor={ques.id + "2"}>
                             {ques.options[2]}
                         </label>
                     </div>
-                    <div className="form-check">
-                        <input className="form-check-input" type="radio" name={ques.id}
-                         id={ques.id+"3"} value="option4" />
-                        <label className="form-check-label" htmlFor={ques.id+"3"}>
-                            {ques.options[3]}
-                        </label>
-                    </div>
+                    {
+                        (ques.options.length===4)?
+                            <div className="form-check">
+                                <input className="form-check-input" type="radio" name={ques.id}
+                                    id={ques.id + "3"} value="option4" />
+                                <label className="form-check-label" htmlFor={ques.id + "3"}>
+                                    {ques.options[3]}
+                                </label>
+                            </div>: <div></div>
+                    }
                 </div>
-                    
+
             }
         </div>
     );
