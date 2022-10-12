0629401331744368ce9cbd4fc6cf4058d4d9f733
AmanatUMahmud <amanatmahmud@gmail.com>
2022-10-12 09:39:31 +0600

    toast added

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const Question = ({ ques, name }) => {
     console.log(ques);
     // const questions = ques.question;
-    // const test = questions.split('<')
-    const handleClickc =() =>{
-
+    // const test = questions.split('<');
+    const [isDisabled,setIsDisabled] = useState(false);
+    const handleClick =(choseen) =>{
+        if(ques.correctAnswer===choseen){
+            toast.success("Correct answer!!!", {autoClose:1000});
+        }
+        else{
+            toast.error("Wrong answer",{autoClose:1000})
+        }
+        setIsDisabled(!isDisabled);
     }
     return (
         <div>
@@ -13,31 +22,38 @@ const Question = ({ ques, name }) => {
             {
                 <div>
                     <div className="form-check">
-                        <input className="form-check-input" type="radio" name={ques.id} id={ques.id} value="option1" />
+                        <input className="form-check-input" type="radio" name={ques.id} id={ques.id} value="option1" onClick={()=>handleClick(ques.options[0])} disabled={isDisabled}
+                        />
                         <label className="form-check-label" htmlFor={ques.id}>
                             {ques.options[0]}
                         </label>
                     </div>
                     <div className="form-check">
                         <input className="form-check-input" type="radio" name={ques.id}
-                            id={ques.id + "1"} value="option2" />
+                        id={ques.id + "1"} value="option2" 
+                        onClick={()=>handleClick(ques.options[1])}/>
                         <label className="form-check-label" htmlFor={ques.id + "1"}>
                             {ques.options[1]}
                         </label>
                     </div>
                     <div className="form-check" 
-                    onClick={handleClickc()}>
+                    >
                         <input className="form-check-input" type="radio" name={ques.id}
-                            id={ques.id + "2"} value="option3" />
+                            id={ques.id + "2"} value="option3" 
+                            onClick={()=>handleClick
+                            (ques.options[2])}/>
                         <label className="form-check-label" htmlFor={ques.id + "2"}>
                             {ques.options[2]}
                         </label>
                     </div>
                     {
                         (ques.options.length===4)?
-                            <div className="form-check">
+                            <div className="form-check"
+                            >
                                 <input className="form-check-input" type="radio" name={ques.id}
-                                    id={ques.id + "3"} value="option4" />
+                                    id={ques.id + "3"} value="option4" 
+                                    onClick={()=>handleClick
+                                    (ques.options[3])}/>
                                 <label className="form-check-label" htmlFor={ques.id + "3"}>
                                     {ques.options[3]}
                                 </label>
@@ -46,6 +62,7 @@ const Question = ({ ques, name }) => {
                 </div>
 
             }
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
